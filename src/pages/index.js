c2a587ed9d2e29bc51646325604ee702c629aef4
Vitaliy Oleinik <74061826+Vitaliy9706@users.noreply.github.com>
2023-05-24 01:15:00 +0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
         <title>Vitaliy Oleinik | Web Developer in Abu Dhabi</title>
         <meta name="description" content="I am a skilled web developer based in Abu Dhabi, specializing in creating engaging and user-friendly websites. I will help you increase your sales" />
       </Head>
-      <TransitionEffect />
+
       <main className='flex items-center text-dark w-full min-h-screen dark:text-light'>
         <Layout className='pt-0 md:pt-12 sm:pt-0'>
           <div className='flex items-center justify-between w-wull lg:flex-col'>
@@ -34,7 +34,7 @@ export default function Home() {
                 }}
             } 
             className='w-1/2 h-[30rem] md:w-full flex flex-col justify-center items-center self-center relative sm:h-[20rem] lg:hidden md:inline-block md:w-full'>
-              <Image src={threedPic} layout="fill" objectFit='cover' objectPosition='center' alt="Vitaliy Oleinik"  />
+              <Image src={threedPic} layout="fill" objectFit='cover' objectPosition='center' loading='eager' alt="Vitaliy Oleinik"  />
             </motion.div>
             <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
               <AnimatedText text="Hello! I'm Vitaliy a web developer based in Abu Dhabi 🇦🇪" className='!text-6xl text-left  xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl' />
